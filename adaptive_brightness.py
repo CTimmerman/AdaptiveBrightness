5f6480d8bee618b779a7fb658f62b692c6ab8f27
Cees Timmerman <CTimmerman@users.noreply.github.com>
2021-05-06 16:05:25 +0200

    Regained focus.

--- adaptive_brightness.py
+++ adaptive_brightness.py
@@ -38,11 +38,13 @@ def main(camera=-1, display=0, debug=False):
         except Exception as ex:
             print(ex)
 
-    if camera < 0:
+    if camera == -1:
         for camera in range(10):
             vc = cv2.VideoCapture(camera)
             if vc.isOpened():
                 break
+    else:
+        vc = cv2.VideoCapture(camera)
 
     if not vc.isOpened():
         print(f"Can't open camera {camera}, {vc}")
