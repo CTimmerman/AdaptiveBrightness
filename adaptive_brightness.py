451e748ef899a1daaf710f060d79e03f3b5caa73
Cees Timmerman <CTimmerman@users.noreply.github.com>
2021-05-06 16:24:27 +0200

    Avoid select() timeout of V4L2 in strangely underpowered (Three 2-core CPUs should do) VMware VM.

--- adaptive_brightness.py
+++ adaptive_brightness.py
@@ -64,14 +64,15 @@ def main(camera=-1, display=0, debug=False):
                 continue
             print(f"Prop {i}, {cv2_prop_name(i)}: {vc.get(i)}")
 
-        rval, frame = vc.read()
-        if rval:
-            cv2.imwrite("debug.jpg", frame)
-
     vc.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
     vc.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
     vc.set(cv2.CAP_PROP_FRAME_WIDTH, 240)
     vc.set(cv2.CAP_PROP_FPS, 1)
+
+    if debug:
+        rval, frame = vc.read()
+        if rval:
+            cv2.imwrite("debug.jpg", frame)
 
     original_brightness = sbc.get_brightness(display)
     brightness = original_brightness
